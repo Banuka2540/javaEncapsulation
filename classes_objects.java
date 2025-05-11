629cf7e73863a198efa58999b9a7209e14c2afa9
Yuthila Banuka <89436149+Banuka2540@users.noreply.github.com>
2025-05-11 23:12:15 +0530

    Exercises given by Lectures
    Exercises given by Lectures

---
+++ classes_objects.java
@@ -0,0 +1,11 @@
+//initialize object variables using reference method
+public class classes_objects {
+    public static void main (String[] args) {
+        Student s1 = new Student() ;
+        s1.name = "yuthila";
+        s1.age = 15 ;
+        System.out.println("your name is : " +s1.name + "  and your age is : " + s1.age);
+
+    }
+
+}
